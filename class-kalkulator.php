d4b404afaf5b636708da2cd6401603238563880a
SarahFlaw <62712271+SarahFlaw@users.noreply.github.com>
2020-03-26 09:34:13 -0700

    Create class-kalkulator.php

---
+++ class-kalkulator.php
@@ -0,0 +1,25 @@
+<?php
+// Class Kalkulator
+class kalkulator{
+
+  function tambah($a, $b){ // Fungsi Pertambahan
+    $out = $a + $b;
+    return $out;
+  }
+  
+  function kurang($a, $b){ // Fungsi Pengurangan
+    $out = $a - $b;
+    return $out;
+  }
+  
+  function bagi($a, $b){ // Fungsi Pembagian
+    $out = $a / $b;
+    return $out;
+  }
+  
+  function kali($a, $b){ // Fungsi Perkalian
+    $out = $a * $b;
+    return $out;
+  }
+  
+}
